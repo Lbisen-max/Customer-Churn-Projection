5c67bead96f902a40b79a81bfeaab4a796ece895
Lbisen-max <lokeshbisen989@gmail.com>
2023-05-01 22:18:12 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -24,13 +24,13 @@ class CustomException(Exception):
 
 
 
-if __name__=="__main__":
+# if __name__=="__main__":
             
-            try:
-                a=1/0
-            except Exception as e:
-                  logging.info("logging has started")
-                  raise CustomException(e,sys)
+#             try:
+#                 a=1/0
+#             except Exception as e:
+#                   logging.info("division by zero")
+#                   raise CustomException(e,sys)
                   
         
             
